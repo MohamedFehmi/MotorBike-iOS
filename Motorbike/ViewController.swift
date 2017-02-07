a5fb113ef39e030b73dac54c190f08ba5b6f304b
Alaa Boukhchina <alaaboukhchina@Alaas-Mac.local>
2017-02-07 13:48:36 -0800

    Initial Commit

---
+++ Motorbike/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Motorbike
+//
+//  Created by Alaa Boukhchina on 2/7/17.
+//  Copyright © 2017 Alaa Boukhchina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
